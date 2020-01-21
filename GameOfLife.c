1ce0117c1c3d695021a1d1cb29e8bc4665a063b4
André Barros <andrebarros1999@gmail.com>
2020-01-21 12:41:15 +0000

    Add files via upload

--- GameOfLife.c
+++ GameOfLife.c
@@ -4,14 +4,12 @@
 #include <Windows.h>
 #include <time.h>
 
-const int SCREEN_WIDTH = 1920;
-const int SCREEN_HEIGHT = 1080;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 
-int ppc = 10; //pixels per cell
-int field[108][192];
-int fieldCopy[108][192];
+int ppc = 4; //pixels per cell
 
 void paintCell(SDL_Renderer* renderer, int ppc, int c, int l) {
 
@@ -24,6 +22,10 @@ void paintCell(SDL_Renderer* renderer, int ppc, int c, int l) {
 
 int main(int argc, char *argv[]) {
     srand(time(NULL));
+    int fieldHeight = SCREEN_HEIGHT / ppc;
+    int fieldWidth = SCREEN_WIDTH / ppc;
+    int *field = malloc(fieldHeight * fieldWidth * sizeof(int));
+    int *fieldCopy = malloc(fieldHeight * fieldWidth * sizeof(int));
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
@@ -32,7 +34,7 @@ int main(int argc, char *argv[]) {
     else
     {
         //Create window
-        window = SDL_CreateWindow("Game Of Life", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
+        window = SDL_CreateWindow("Game Of Life", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, NULL);
         if (window == NULL)
         {
             printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
@@ -47,16 +49,16 @@ int main(int argc, char *argv[]) {
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         //SDL RendererClear sets entire window to color specified in renderer draw color
     }
-    for(int c = 0; c < SCREEN_WIDTH / ppc; c++) {
-        for(int l = 0; l < SCREEN_HEIGHT / ppc; l++) {
-            if (rand() % 2 == 0) {
-                field[l][c] = 1;
-                fieldCopy[l][c] = 1;
+    for(int c = 0; c < fieldWidth; c++) {
+        for(int l = 0; l < fieldHeight; l++) {
+            if (rand() % 3 == 0) {
+                field[l * fieldWidth + c] = 1;
+                fieldCopy[l * fieldWidth + c] = 1;
                 paintCell(renderer, ppc, c, l);
             }
             else {
-                field[l][c] = 0;
-                fieldCopy[l][c] = 0;
+                field[l * fieldWidth + c] = 0;
+                fieldCopy[l * fieldWidth + c] = 0;
             }
         }
     }
@@ -64,8 +66,8 @@ int main(int argc, char *argv[]) {
     int generations = 0;
     printf("%i\n", generations);
     while (1) {
-        for (int c = 0; c < SCREEN_WIDTH / ppc; c++) {
-            for (int l = 0; l < SCREEN_HEIGHT / ppc; l++) {
+        for (int c = 0; c < fieldWidth; c++) {
+            for (int l = 0; l < fieldHeight; l++) {
                 SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
                 int AliveNeighbors = 0;
                 for (int nc = c - 1; nc <= c + 1; nc++) {
@@ -73,19 +75,19 @@ int main(int argc, char *argv[]) {
                         int nlav = nl;
                         int ncav = nc;
                         if (nl < 0) {
-                            nl = SCREEN_HEIGHT / ppc - 1;
+                            nl = fieldHeight - 1;
                         }
-                        if (nl > SCREEN_HEIGHT / ppc - 1) {
+                        if (nl > fieldHeight - 1) {
                             nl = 0;
                         }
                         if (nc < 0) {
-                            nc = SCREEN_WIDTH / ppc - 1;
+                            nc = fieldWidth - 1;
                         }
-                        if (nc > SCREEN_WIDTH / ppc - 1) {
+                        if (nc > fieldWidth - 1) {
                             nc = 0;
                         }
                         if (nl != l || nc != c) {
-                            if (field[nl][nc] == 1) {
+                            if (field[nl * fieldWidth + nc] == 1) {
                                 AliveNeighbors++;
                             }
                         }
@@ -94,19 +96,19 @@ int main(int argc, char *argv[]) {
                     }
                 }
                 if (AliveNeighbors < 2 || AliveNeighbors > 3) {
-                    fieldCopy[l][c] = 0;
+                    fieldCopy[l * fieldWidth + c] = 0;
                     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
                     paintCell(renderer, ppc, c, l);
                 }
                 if (AliveNeighbors == 3) {
-                    fieldCopy[l][c] = 1;
+                    fieldCopy[l * fieldWidth + c] = 1;
                     paintCell(renderer, ppc, c, l);
                 }
             }
         }
-        for (int c = 0; c < SCREEN_WIDTH / ppc; c++) {
-            for (int l = 0; l < SCREEN_HEIGHT / ppc; l++) {
-                field[l][c] = fieldCopy[l][c];
+        for (int c = 0; c < fieldWidth; c++) {
+            for (int l = 0; l < fieldHeight; l++) {
+                field[l * fieldWidth + c] = fieldCopy[l * fieldWidth + c];
             }
         }
         SDL_RenderPresent(renderer);
@@ -115,6 +117,8 @@ int main(int argc, char *argv[]) {
         //Sleep(500);
         //Sleep(x) -> wait x miliseconds before continuing
     }
+    free(field);
+    free(fieldCopy);
     return 0;
 }
     
